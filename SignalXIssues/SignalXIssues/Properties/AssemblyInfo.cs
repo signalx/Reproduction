cf51a7edfbc22442c475be79f51d8cbe0935e42b
Sam <contactsamie@gmail.com>
2018-08-19 19:38:59 -0700

    add sample test

---
+++ SignalXIssues/SignalXIssues/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SignalXIssues")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SignalXIssues")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4affaaa8-82c5-4249-822d-d8026d560849")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SignalXIssues/SignalXIssues/issue_test_template.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SignalXIssues
+{
+    using SignalXLib.Lib;
+    using SignalXLib.TestHelperLib;
+
+    [TestClass]
+    public class issue_test_template
+    {
+        [TestMethod]
+        public void it_should_call_server_from_client()
+        {
+         
+            TestHelper.MaxTestWaitTime = TimeSpan.FromSeconds(20);
+            var scenario = new ScenarioDefinition(
+                script: @"signalx.ready(function (server) {
+							      server.myServer('abc',function (message) {
+							       });
+                                 });",
+                server: () =>
+                {
+                    SignalX.Server("myServer",
+                        request =>
+                        {
+                            SignalX.RespondToAll(request.ReplyTo, request.Message);
+                        });
+                },
+                checks: () =>
+                {
+                    Assert.IsTrue(true);
+                }
+            );
+            TestHelper.RunScenario(scenario);
+        }
+    }
+}
